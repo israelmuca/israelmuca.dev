beec1133b72f8a893f42842ab7f546d93fa4d9be
Israel Muñoz <isra.munoz@gmail.com>
2019-02-20 10:00:19 -0600

    Started making changes after receiving the new design

--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,31 @@
 <template>
-<div>
-  <div class="columns is-mobile is-centered">
-    <div class="column is-half">
-      <p>Proident qui aute nisi officia eiusmod proident duis. Sit nulla id excepteur consequat ut cupidatat do officia. Lorem ex cupidatat sint ipsum quis ex exercitation.</p>
-      <br>
-      <p>Exercitation eu aliquip nulla elit laborum deserunt laboris nisi in ex reprehenderit. Ipsum esse laboris culpa sint ullamco enim magna sunt ut tempor voluptate elit dolor pariatur. Consectetur sint ipsum qui aliquip enim. Occaecat eu anim non pariatur ea excepteur nisi deserunt in ea. Nostrud mollit culpa dolor elit enim nostrud consequat voluptate adipisicing quis eu laborum veniam id.</p>
-    </div>
-  </div>
-
-
-  <div class="columns"></div>
-  <div class="columns"></div>
-  <div class="columns"></div>
-
-  <nav class="level">
-    <p class="level-item">
-      <a href="https://www.github.com/IsraelMuCa" target="_blank" rel="noopener noreferrer" aria-label="Githubs"><fab :icon="fab.faGithub" /></a>
-    </p>
-    <p class="level-item">
-      <a href="https://www.twitter.com/IsraelMuCa" target="_blank" rel="noopener noreferrer" aria-label="Twitter"><fab :icon="fab.faTwitter" /></a>
-    </p>
-  </nav>
-</div>
+  <header></header>
 </template>
 
 <script>
-import { fab } from '@fortawesome/free-brands-svg-icons'
+const Header = () => import("~/components/header");
 
 export default {
-  computed: {
-    fab () {
-      return fab
-    }
+  head() {
+    return {
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Landing page with blog for IsraelMuCa"
+        },
+        {
+          hid: "keywords",
+          name: "keywords",
+          content:
+            "Full Stack full-stack node.js nodejs express vue nuxt landing blog javascript jamstack"
+        }
+      ]
+    };
   }
-  
-}
+};
 </script>
 
 
 <style lang="css" scoped>
-
-.columns {
-  margin-top: 4rem;
-}
-
-a {
-  font-size: 2rem;
-  color: black;
-  text-decoration: none;
-}
 </style>
