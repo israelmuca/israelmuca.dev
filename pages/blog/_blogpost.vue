d8f8afd273c5b7b330ed9c576befa0c75f429891
Israel Muñoz <isra.munoz@gmail.com>
2019-02-22 14:35:44 -0600

    Started styling blog pages, and modified the Netlify CMS field properties

--- pages/blog/_blogpost.vue
+++ pages/blog/_blogpost.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    <nuxtdown-body class="body" :body="post.body"/>
+    <Header>
+      <h1 class="title">{{ post.title }}</h1>
+      <h2 class="subtitle">{{ post.summary }}</h2>
+    </Header>
+    <div class="container">
+      <nuxtdown-body class="body" :body="post.body"/>
+    </div>
   </div>
 </template>
 
 <script>
+import Header from "~~/components/header.vue";
+
 export default {
+  components: {
+    Header
+  },
   head: function() {
     return {
       title: `${this.post.title}`,
@@ -14,6 +25,11 @@ export default {
           hid: "description",
           name: "description",
           content: this.post.description
+        },
+        {
+          hid: "keywords",
+          name: "keywords",
+          content: this.post.keywords
         }
       ]
     };
@@ -25,3 +41,21 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.title {
+	color: #FFFFFF;
+	font-family: "Eczar";
+  font-weight: 500;
+	font-size: 48px;
+	line-height: 64px;
+}
+
+.subtitle {
+	color: #FFFFFF;
+	font-family: "Work Sans";
+	font-size: 21px;
+	letter-spacing: -0.88px;
+	line-height: 35px;
+}
+</style>
