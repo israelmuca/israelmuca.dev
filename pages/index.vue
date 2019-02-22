38107fa7646b9ddd9a31e233622902380a947504
Israel Muñoz <isra.munoz@gmail.com>
2019-02-22 10:59:29 -0600

    Changed some settings in the CMS

--- pages/index.vue
+++ pages/index.vue
@@ -14,9 +14,9 @@
 </template>
 
 <script>
-import Header from '~~/components/header.vue'
-import ArticleCard from '~~/components/articleCard.vue'
-import Footer from '~~/components/footer.vue'
+import Header from "~~/components/header.vue";
+import ArticleCard from "~~/components/articleCard.vue";
+import Footer from "~~/components/footer.vue";
 
 export default {
   components: {
